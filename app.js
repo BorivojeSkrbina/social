e8aa4ddba5fa6d12b3556f52efad2f2bac2cbea0
Borivoje Skrbina <borivoje.s@gmail.com>
2018-04-17 01:31:08 +0200

    code optimisation - todo suggested friends

--- app.js
+++ app.js
@@ -79,26 +79,17 @@ http.get('data.json')
     }
 
 
-
-
     // All Users Loop
-    users.forEach((singleUser) => {
-      const id = singleUser.id,
-        firstName = singleUser.firstName,
-        surname = singleUser.surname,
-        friends = singleUser.friends;
+    users.forEach((u) => {
       // Instantiate user
-      const user = new User(id, firstName, surname, friends);
+      const user = new User(u.id, u.firstName, u.surname, u.friends);
       // Instantiate UI
       const ui = new UI();
       // Add user
       ui.addUser(user);
     });
 
-
-
-
-
+    
     // ----------------------------------------------- Event Listener for Selecting User
     document.getElementById('book-list').addEventListener('click', (e) => {
 
@@ -140,73 +131,7 @@ http.get('data.json')
           }
         });
 
-
-
-        // selectedObject.friends.forEach((friend) => {
-        //   // console.log(singleUser);
-        //   const id = users[friend - 1].id,
-        //     firstName = users[friend - 1].firstName,
-        //     surname = users[friend - 1].surname,
-        //     friends = users[friend - 1].friends;
-        //   // Instantiate user
-        //   const user = new User(id, firstName, surname, friends);
-        //   // Instantiate UI
-        //   const ui = new UI();
-        //   // Select user
-        //   ui.selectUser(user);
-        // });
-
-
         // ------------------------------------------------- Friends of friends Loop
-        // let niz = [];
-        // selectedObject.friends.forEach((friends) => {
-        //   niz.push(users[friends - 1].id);
-        // });
-
-        // let niz1 = [];
-
-        // niz.forEach((friend) => {
-        //   users[friend - 1].friends.forEach((fr) => {
-        //     if (niz1.indexOf(fr) == -1) {
-        //       niz1.push(fr);
-        //     }
-        //   });
-        // });
-
-        // niz1.forEach((fr) => {
-
-        //   if (fr == selectedId) {
-        //     niz1.splice(niz1.indexOf(fr), 1);
-        //   }
-
-        // });
-
-        // niz.forEach((fr1) => {
-        //   niz1.forEach((fr) => {
-
-        //     if (fr === selectedId) {
-        //       niz1.splice(niz1.indexOf(fr), 1);
-        //     }
-
-        //     if (fr === fr1) {
-        //       niz1.splice(niz1.indexOf(fr), 1);
-        //     }
-
-        //   });
-        // });
-
-        // niz1.forEach((suggested) => {
-        //   const id = users[suggested - 1].id,
-        //     firstName = users[suggested - 1].firstName,
-        //     surname = users[suggested - 1].surname,
-        //     friends = users[suggested - 1].friends;
-        //   // Instantiate user
-        //   const user = new User(id, firstName, surname, friends);
-        //   // Instantiate UI
-        //   const ui = new UI();
-        //   // Select user
-        //   ui.selectDirectFriends(user);
-        // });
 
 
         let friendsOfFriends = [];
@@ -218,8 +143,11 @@ http.get('data.json')
             // and not equal to selectedObject.id
             // and who is not in array already
             if (
+              // Dodaj ako izabrani user zna trenutnog usera
               selectedObject.friends.indexOf(user.id) !== -1 &&
+              // Nemoj da dodas ako prijatelji trenutnog usera znaju izabranog usera
               selectedObject.id !== friend_id &&
+              // Nemoj da dodas ako se vec nalazi u nizu
               friendsOfFriends.indexOf(friend_id) === -1
             ) {
               friendsOfFriends.push(friend_id);
@@ -240,110 +168,117 @@ http.get('data.json')
             // Select user
             ui.selectDirectFriends(user);
           }
-          
+
         });
 
         // ------------------------------------------------ Suggested Friends Loop
 
-        // let arr = [];
-        // users.forEach((user) => {
-        //   if (user.friends.length > 1) {
-        //     arr.push(user);
-        //   }
-        // });
-
-        // let arr1 = [];
-        // arr.forEach((user) => {
-
-        //   let x = 0;
-        //   user.friends.forEach((id) => {
-
-        //     if (selectedId == id) {
-        //       x = 1;
-        //     }
-
-        //   });
-
-        //   if (x === 0) {
-        //     arr1.push(user);
-        //   }
-
-        // });
-
-        // arr1.splice(arr1.indexOf(selectedObject), 1);
-
-
-        // let suggestedFriendsArray = [];
-        // arr1.forEach((user) => {
-
-        //   let x = 0;
-        //   user.friends.forEach((fost) => {
-
-        //     selectedObject.friends.forEach((id) => {
-        //       if (fost === id) {
-        //         x += 1;
-        //       }
-
-        //     });
-
-        //   });
-
-        //   if (x > 1) {
-        //     suggestedFriendsArray.push(user.id);
-        //   }
-
-        // });
-
-        // suggestedFriendsArray.forEach((suggested) => {
-        //   const id = users[suggested - 1].id,
-        //     firstName = users[suggested - 1].firstName,
-        //     surname = users[suggested - 1].surname,
-        //     friends = users[suggested - 1].friends;
-        //   // Instantiate user
-        //   const user = new User(id, firstName, surname, friends);
-        //   // Instantiate UI
-        //   const ui = new UI();
-        //   // Select user
-        //   ui.selectSuggestedFriends(user);
-        // });
-
-        console.log(friendsOfFriends);
-
-
-        let suggestedFriends = [];
-        // loop trough all users
-        users.forEach(user => {
-          // go to user friend list
-          user.friends.forEach(friend_id => {
-            // get ids of friends of friend who are in selectedObject.friends
-            // and not equal to selectedObject.id
-            // and who is not in array already
-            if (
-              selectedObject.friends.indexOf(user.id) !== -1 &&
-              selectedObject.id !== friend_id &&
-              suggestedFriends.indexOf(friend_id) === -1
-              // && users.suggestedFriends.friends.length > 2
-            ) {
-              suggestedFriends.push(friend_id);
-            }
-          });
-        });
-        // remove direct
-        suggestedFriends = suggestedFriends.filter(fri => {
-          return selectedObject.friends.indexOf(fri) === -1;
-        });
-
-
-        users.forEach(u => {
-          if (suggestedFriends.indexOf(u.id) !== -1) {
-            const user = new User(u.id, u.firstName, u.surname, u.friends);
-            // Instantiate UI
-            const ui = new UI();
-            // Select user
-            ui.selectSuggestedFriends(user);
+        let arr = [];
+        // Niz svih usera sa 2 i vise prijatelja
+        users.forEach((user) => {
+          if (user.friends.length > 1) {
+            arr.push(user);
           }
-          
         });
+
+        console.log(arr);
+
+        let arr1 = [];
+        // Niz usera koji ne znaju izabranog usera
+        arr.forEach((user) => {
+
+          let x = 0;
+          user.friends.forEach((id) => {
+            // Gleda da li user zna izabranog usera i poveceva x
+            if (selectedId == id) {
+              x = 1;
+            }
+
+          });
+
+          // Ukoliko ga ne zna dodaje ga u niz
+          if (x === 0) {
+            arr1.push(user);
+          }
+
+        });
+
+        // Izbacuje iz niza selektovanog usera
+        arr1.splice(arr1.indexOf(selectedObject), 1);
+
+
+        let suggestedFriendsArray = [];
+
+        // Gleda da li user zna direktnog prijatelja izabranog usera
+        arr1.forEach((user) => {
+
+          let x = 0;
+          user.friends.forEach((fost) => {
+            // Uporedjivanje direktnih prijatelja sa prijateljima izabranog usera
+            selectedObject.friends.forEach((id) => {
+              if (fost === id) {
+                x += 1;
+              }
+
+            });
+
+          });
+          // Ukoliko je broj 2 ili vise dodaj u konacni niz
+          if (x > 1) {
+            suggestedFriendsArray.push(user.id);
+          }
+
+        });
+
+        suggestedFriendsArray.forEach((suggested) => {
+          const id = users[suggested - 1].id,
+            firstName = users[suggested - 1].firstName,
+            surname = users[suggested - 1].surname,
+            friends = users[suggested - 1].friends;
+          // Instantiate user
+          const user = new User(id, firstName, surname, friends);
+          // Instantiate UI
+          const ui = new UI();
+          // Select user
+          ui.selectSuggestedFriends(user);
+        });
+
+
+
+        // let suggestedFriends = [];
+        // // loop trough all users
+        // users.forEach(user => {
+        //   // go to user friend list
+        //   user.friends.forEach(friend_id => {
+        //     // get ids of friends of friend who are in selectedObject.friends
+        //     // and not equal to selectedObject.id
+        //     // and who is not in array already
+        //     if (
+        //       selectedObject.friends.indexOf(user.id) !== -1 &&
+        //       selectedObject.id !== friend_id &&
+        //       suggestedFriends.indexOf(friend_id) === -1
+        //       // && users.suggestedFriends.friends.length > 2
+        //     ) {
+        //       suggestedFriends.push(friend_id);
+        //     }
+        //   });
+        // });
+        // // remove direct
+        // suggestedFriends = suggestedFriends.filter(fri => {
+        //   return selectedObject.friends.indexOf(fri) === -1;
+        // });
+
+
+        // users.forEach(u => {
+        //   if (suggestedFriends.indexOf(u.id) !== -1) {
+        //     const user = new User(u.id, u.firstName, u.surname, u.friends);
+        //     // Instantiate UI
+        //     const ui = new UI();
+        //     // Select user
+        //     ui.selectSuggestedFriends(user);
+        //   }
+
+        // });
 
 
 
